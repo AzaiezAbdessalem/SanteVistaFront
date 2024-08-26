3748535d008f97c6ead4fa721c3371d8e0fce4c1
AzaiezAbdessalem <azaiezabdessalem@gmail.com>
2024-08-26 15:02:27 +0100

    up

--- src/app/components/fiche-desuivi/fiche-desuivi.component.ts
+++ src/app/components/fiche-desuivi/fiche-desuivi.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import dayGridPlugin from '@fullcalendar/daygrid'; // Import dayGridPlugin
+import interactionPlugin from '@fullcalendar/interaction'; // Import interactionPlugin
 import { ActivatedRoute } from '@angular/router';
 import { Regime } from 'src/app/class/regime';
 import { RegimeService } from 'src/app/service/regime.service';
@@ -13,7 +15,6 @@ import { ActivityServiceService } from 'src/app/service/activity.service';
   styleUrls: ['./fiche-desuivi.component.css']
 })
 export class FicheDesuiviComponent implements OnInit {
-
   userId: string | null = null;
   User: User | null = null;
   regimes: Regime[] = [];
@@ -25,7 +26,30 @@ export class FicheDesuiviComponent implements OnInit {
 
   constructor(private userService: UserService, private route: ActivatedRoute, private regimeService: RegimeService,private activityService:ActivityServiceService) {}
 
+  // Liste des événements
+  events = [
+    {
+      title: 'Rendez-vous 1',
+      start: '2024-08-12T10:30:00',
+      end: '2024-08-12T11:00:00'
+    },
+    {
+      title: 'Rendez-vous 2',
+      start: '2024-09-15T14:00:00',
+      end: '2024-08-15T15:00:00'
+    },
+    {
+      title: 'Rendez-vous 3',
+      start: '2024-08-20T09:00:00',
+      end: '2024-08-20T10:00:00'
+    }
+  ];
+
+
+  calendarOptions: any;
+
   ngOnInit(): void {
+
     this.userId = this.route.snapshot.paramMap.get('id');
     console.log(this.userId);
 
@@ -44,7 +68,53 @@ export class FicheDesuiviComponent implements OnInit {
     this.getAllRegimes();
     this.getAllActivities();
     
+    this.getAllRegimes();    const monthsWithEvents = this.getMonthsWithEvents(this.events);
+
+    this.calendarOptions = {
+      initialView: 'dayGridMonth',
+      plugins: [dayGridPlugin, interactionPlugin],
+      editable: true,
+      locale: 'fr',
+      events: this.events,
+      eventColor: '#4caf50',
+      visibleRange: {
+        start: monthsWithEvents.startDate,
+        end: monthsWithEvents.endDate
+      },
+      headerToolbar: {
+        right: 'prev,next', // Boutons de navigation (précédent, suivant)
+         left: 'title' // Rien à droite pour enlever le bouton Today
+      }
+    };
   }
+
+  // Méthode pour obtenir les mois contenant des événements
+  getMonthsWithEvents(events: any[]): any {
+    let startMonth=  new Date ()
+    let endMonth=  new Date () 
+
+    events.forEach(event => {
+      const eventStartDate = new Date(event.start);
+      const eventEndDate = new Date(event.end);
+
+      if (!startMonth || eventStartDate < startMonth) {
+        startMonth = new Date(eventStartDate.getFullYear(), eventStartDate.getMonth(), 1);
+      }
+
+      if (!endMonth || eventEndDate > endMonth) {
+        endMonth = new Date(eventEndDate.getFullYear(), eventEndDate.getMonth() + 1, 0);
+      }
+    });
+
+    return {
+      startDate: startMonth ? startMonth.toISOString().split('T')[0] : '',
+      endDate: endMonth ? endMonth.toISOString().split('T')[0] : ''
+    };
+  }
+  AddRendezVous()
+  {}
+
+
 
   getAllRegimes(): void {
     this.regimeService.getAllRegimes().subscribe((data: Regime[]) => {
